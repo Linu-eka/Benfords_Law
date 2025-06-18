78f62c9d67cc5960e3d047572651afd369e0fa49
leka0001 <leka0001@student.monash.edu>
2025-06-18 23:40:48 +1000

    feat: benfords law calculation added.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 from tkinter import filedialog as fd
 from process_file import ProcessFile
+from benfords import calculate_benfords
 
-def select_file(filename_label):
+def select_file(filename_label, result_text):
     filetypes = (
         ("CSV files", "*.csv"),
     )
@@ -16,7 +17,18 @@ def select_file(filename_label):
     if file:
         filename_label.config(text=f"Selected: {file[0].name}")
         amounts = ProcessFile(file[0].name)
-        print("Amounts extracted:", amounts)  # For debugging purposes
+        total_items, comparison_result = calculate_benfords(amounts)
+        result_text.config(state='normal')
+        result_text.delete(1.0, tk.END)
+
+        formatted = ""
+        # Format the comparison_result dictionary for display
+        for digit, data in comparison_result.items():
+            formatted += f"Digit: {digit} Expected: {data['expected']:.2f}% Real: {data['real']:.2f}% Difference: {data['difference']:.2f}%\n"
+
+        result_text.insert(tk.END, formatted)
+        result_text.config(state='disabled')
+
 
     else:
         filename_label.config(text="No file selected")
@@ -25,18 +37,22 @@ def create_window():
     window = tk.Tk()
     window.title("Benfords Law Detector")
 
-    window.geometry("800x300")
+    window.geometry("800x400")
     window.configure(bg="lightblue")
     label = tk.Label(window, text="Benford's Law Detector", bg="lightblue", font=("Times New Roman", 12))
     file_button = tk.Button(window, text="Select CSV File", bg="white", font=("Times New Roman", 10))
     filename_label = tk.Label(window, text="No file selected", bg="lightblue", font=("Times New Roman", 10))
-    
-    file_button.config(command=lambda: select_file(filename_label))
+    result_label = tk.Label(window, text="Results:", bg="lightblue", font=("Times New Roman", 12))
+    result_text = tk.Text(window, height=10, width=80, bg="white", font=("Times New Roman", 10), state='disabled')
+    file_button.config(command=lambda: select_file(filename_label,result_text))
     
     label.pack(pady=20)
     file_button.pack(pady=20)
     
     filename_label.pack()
+    result_label.pack(pady=10,padx=10,anchor='w')
+    result_text.pack(pady=10,padx=10, anchor='w')
+
 
     
 
